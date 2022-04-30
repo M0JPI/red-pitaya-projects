28dd1676346a3df72b8eeb17a001f0a67b13344d
John Bracegirdle <john.p.bracegirdle@gmail.com>
2022-04-30 20:47:01 +0000

    MultiChannel Gas Senor V2 Version 1

--- jupyter-notebooks/dashboard/bme680.py
+++ jupyter-notebooks/dashboard/bme680.py
@@ -20,7 +20,7 @@
 ##OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-import constants
+import bme680_constants as constants
 import math
 import time
 
--- jupyter-notebooks/dashboard/gas_gmxxxb.py
+++ jupyter-notebooks/dashboard/gas_gmxxxb.py
@@ -21,17 +21,14 @@
 ##    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 ##    THE SOFTWARE.1  USA
     
-import math
-import time
+
+from smbus2 import SMBus, i2c_msg
+#import time
+#import math
 
 __version__ = '1.1.1'
 
-class GAS_GMXXX:
-    isPreheated = False
-    GMXXX_ADDRESS = 0x08
-    GMXXX_CMD_REG = 0x00
-    GM_VERF = 3.3
-    GM_RESOLUTION = 1023
+class GAS_GMXXX():
     GM_102B = 0x01
     GM_302B = 0x03
     GM_502B = 0x05
@@ -39,54 +36,69 @@ class GAS_GMXXX:
     CHANGE_I2C_ADDR = 0x55
     WARMING_UP = 0xFE
     WARMING_DOWN = 0xFF
+    
+    i2c_address=0x08
+    isPreheated = True
+    _i2c=None
+    
+    def __init__(self,i2c_addr=0x08, i2c_device=None, isPreheated = True):
+        """Initialise GAS_GMXXX sensor instance.
 
-
-
-    def __init__(self, i2c_addr=GMXXX_ADDRESS, i2c_device=None):
-        """Initialise GAS_GMXXX sensor instance and verify device presence.
-
-        :param i2c_addr: i2c address of GAS_GMXXX
+        :param i2c_addr: Optional i2c address of GAS_GMXXX
         :param i2c_device: Optional SMBus-compatible instance for i2c transport
-
+        :param isPreheated: Optional turn on the heater
         """
-        self.i2c_addr = i2c_addr
+        self.i2c_address = i2c_addr
         self._i2c = i2c_device
         if self._i2c is None:
-            import smbus
-            self._i2c = smbus.SMBus(0)
-
+            self._i2c = SMBus(0)
+        
+        self.isPreheated = isPreheated
+        if self.isPreheated == True:
+            self.preheated()
+        else:
+            self.unPreheated()
+        print("Gas Sensors setup: ",isPreheated)
+        
     def preheated(self):
-        _set_regs(GMXXX_CMD_REG,WARMING_UP);
-        isPreheated = True;
+        msg = i2c_msg.write(self.i2c_address, [self.WARMING_UP])
+        self._i2c.i2c_rdwr(msg)
+        self.isPreheated = True
+        return True
     
     def unPreheated(self):
-        _set_regs(GMXXX_CMD_REG,WARMING_DOWN);
-        isPreheated = False;
+        msg = i2c_msg.write(self.i2c_address, [self.WARMING_DOWN])
+        self._i2c.i2c_rdwr(msg)
+        self.isPreheated = False
+        return True
+    
+    def calcVol(self, adc_value, verf = 3.3, resolution = 1023):
+        return (adc_value * verf) / (resolution * 1.0)
+    
+    def getGM102B_raw(self):
+        return int.from_bytes(self._i2c.read_i2c_block_data(self.i2c_address, self.GM_102B,2), byteorder='little')
+    
+    def getGM102B_volts(self):
+        return self.calcVol(self.getGM102B_raw())
 
-    def _set_bits(self, register, mask, position, value):
-        """Mask out and set one or more bits in a register."""
-        temp = self._get_regs(register, 1)
-        temp &= ~mask
-        temp |= value << position
-        self._set_regs(register, temp)
-
-    def _set_regs(self, register, value):
-        """Set one or more registers."""
-        if isinstance(value, int):
-            self._i2c.write_byte_data(self.i2c_addr, register, value)
-        else:
-            self._i2c.write_i2c_block_data(self.i2c_addr, register, value)
-
-    def _get_regs(self, register, length):
-        """Get one or more registers."""
-        if length == 1:
-            return self._i2c.read_byte_data(self.i2c_addr, register)
-        else:
-            return self._i2c.read_i2c_block_data(self.i2c_addr, register, length)
-        
-    def getGM102B(self):
-        """Get one or more registers."""
-        if length == 1:
-            return self._i2c.read_byte_data(self.i2c_addr, register)
-        else:
-            return self._i2c.read_i2c_block_data(self.i2c_addr, register, length)
+    def getGM302B_raw(self):
+        return int.from_bytes(self._i2c.read_i2c_block_data(self.i2c_address, self.GM_302B,2), byteorder='little')
+    
+    def getGM302B_volts(self):
+        return self.calcVol(self.getGM302B_raw())   
+ 
+    def getGM502B_raw(self):
+        return int.from_bytes(self._i2c.read_i2c_block_data(self.i2c_address, self.GM_502B,2), byteorder='little')
+    
+    def getGM502B_volts(self):
+        return self.calcVol(self.getGM502B_raw())
+ 
+    def getGM702B_raw(self):
+        return int.from_bytes(self._i2c.read_i2c_block_data(self.i2c_address, self.GM_702B,2), byteorder='little')
+    
+    def getGM702B_volts(self):
+        return self.calcVol(self.getGM702B_raw())
+    
+    def change_I2C_Address(self, newAddress):
+        self._i2c.write_byte_data(self.i2c_address, self.CHANGE_I2C_ADDR, newAddress)
+        self.i2c_address = newAddress
