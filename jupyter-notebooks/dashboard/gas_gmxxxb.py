f954d7c1217916ffa26535454075460d576c7fde
John Bracegirdle <john.p.bracegirdle@gmail.com>
2022-04-30 17:36:42 +0000

    MultiChannel Gas Senor V2 updates

---
+++ jupyter-notebooks/dashboard/gas_gmxxxb.py
@@ -0,0 +1,92 @@
+##    Multichannel_Gas_GMXXX.cpp
+##    Description: A drive for Seeed Grove Multichannel gas sensor V2.0.
+##    2019 Copyright (c) Seeed Technology Inc.  All right reserved.
+##    Author: Hongtai Liu(lht856@foxmail.com) ported to Python by John Bracegirdle M0JPI
+##    2019-6-18
+
+##    The MIT License (MIT)
+##    Permission is hereby granted, free of charge, to any person obtaining a copy
+##    of this software and associated documentation files (the "Software"), to deal
+##    in the Software without restriction, including without limitation the rights
+##    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+##    copies of the Software, and to permit persons to whom the Software is
+##    furnished to do so, subject to the following conditions:
+##    The above copyright notice and this permission notice shall be included in
+##    all copies or substantial portions of the Software.
+##    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+##    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+##    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+##    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+##    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+##    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+##    THE SOFTWARE.1  USA
+    
+import math
+import time
+
+__version__ = '1.1.1'
+
+class GAS_GMXXX:
+    isPreheated = False
+    GMXXX_ADDRESS = 0x08
+    GMXXX_CMD_REG = 0x00
+    GM_VERF = 3.3
+    GM_RESOLUTION = 1023
+    GM_102B = 0x01
+    GM_302B = 0x03
+    GM_502B = 0x05
+    GM_702B = 0x07
+    CHANGE_I2C_ADDR = 0x55
+    WARMING_UP = 0xFE
+    WARMING_DOWN = 0xFF
+
+
+
+    def __init__(self, i2c_addr=GMXXX_ADDRESS, i2c_device=None):
+        """Initialise GAS_GMXXX sensor instance and verify device presence.
+
+        :param i2c_addr: i2c address of GAS_GMXXX
+        :param i2c_device: Optional SMBus-compatible instance for i2c transport
+
+        """
+        self.i2c_addr = i2c_addr
+        self._i2c = i2c_device
+        if self._i2c is None:
+            import smbus
+            self._i2c = smbus.SMBus(0)
+
+    def preheated(self):
+        _set_regs(GMXXX_CMD_REG,WARMING_UP);
+        isPreheated = True;
+    
+    def unPreheated(self):
+        _set_regs(GMXXX_CMD_REG,WARMING_DOWN);
+        isPreheated = False;
+
+    def _set_bits(self, register, mask, position, value):
+        """Mask out and set one or more bits in a register."""
+        temp = self._get_regs(register, 1)
+        temp &= ~mask
+        temp |= value << position
+        self._set_regs(register, temp)
+
+    def _set_regs(self, register, value):
+        """Set one or more registers."""
+        if isinstance(value, int):
+            self._i2c.write_byte_data(self.i2c_addr, register, value)
+        else:
+            self._i2c.write_i2c_block_data(self.i2c_addr, register, value)
+
+    def _get_regs(self, register, length):
+        """Get one or more registers."""
+        if length == 1:
+            return self._i2c.read_byte_data(self.i2c_addr, register)
+        else:
+            return self._i2c.read_i2c_block_data(self.i2c_addr, register, length)
+        
+    def getGM102B(self):
+        """Get one or more registers."""
+        if length == 1:
+            return self._i2c.read_byte_data(self.i2c_addr, register)
+        else:
+            return self._i2c.read_i2c_block_data(self.i2c_addr, register, length)
